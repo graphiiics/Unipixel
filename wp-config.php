407368e643b49b5a14cab7742391ca3ab4c50480
Velkan Gael Manzanares <gaelgeovanimq@gmail.com>
2015-05-24 19:49:34 -0500

    v2
    Cambio de db

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'unipixel');
+define('DB_NAME', 'unipixeldb');
 
 /** MySQL database username */
 define('DB_USER', 'gael');
